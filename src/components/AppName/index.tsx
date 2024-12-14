7449bc03473f04cbf8e13d5d7210d2fbbfa9845d
estefane.cortes <estefane.cortes@aquare.la>
2024-12-14 19:29:59 -0300

    refactor: create size prop for AppName component

--- src/components/AppName/index.tsx
+++ src/components/AppName/index.tsx
@@ -1,10 +1,25 @@
 import { Kanban } from 'lucide-react'
 
-export function AppName() {
+type Props = {
+  size?: 'small' | 'large'
+}
+
+export function AppName({ size = 'large' }: Props) {
+  const theme = {
+    small: {
+      fontSize: '1.25rem',
+      iconSize: 32
+    },
+    large: {
+      fontSize: '1.875rem',
+      iconSize: 42
+    }
+  }
+
   return (
-    <h1 className="flex items-center justify-center gap-2 text-3xl font-bold">
-      <Kanban size={42} />
-      To-Do List
+    <h1 className="flex min-w-max items-center justify-center gap-2 font-bold">
+      <Kanban size={theme[size].iconSize} />
+      <p style={{ fontSize: theme[size].fontSize }}>To-Do List</p>
     </h1>
   )
 }
