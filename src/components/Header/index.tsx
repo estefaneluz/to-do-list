0b2c07b6e55e78413f56697813c6905c5593ee29
estefane.cortes <estefane.cortes@aquare.la>
2024-12-14 19:30:12 -0300

    feat: create Header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+import { Plus } from 'lucide-react'
+import { AppName } from '../AppName'
+import { Button } from '../ui/button'
+import { Input } from '../ui/input'
+
+export function Header() {
+  return (
+    <header className="flex items-center gap-4 border-b p-4">
+      <AppName size="small" />
+      <Input type="text" placeholder="Search tasks..." />
+
+      <Button className="mr-auto">
+        <Plus size={16} />
+        New Task
+      </Button>
+    </header>
+  )
+}
