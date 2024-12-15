74b80941099c374c791d6d06bec01173fa178e14
estefane.cortes <estefanecortes@gmail.com>
2024-12-15 18:37:40 -0300

    feat: update select component

--- src/components/Fields/Select/index.tsx
+++ src/components/Fields/Select/index.tsx
@@ -1,4 +1,5 @@
 import { Label } from '@/components/ui/label'
+import clsx from 'clsx'
 import { default as ReactSelect } from 'react-select'
 
 type Props = {
@@ -9,7 +10,23 @@ export const Select = ({ label, ...props }: Props) => {
   return (
     <div className="grid w-full gap-1.5">
       {!!label && <Label>{label}</Label>}
-      <ReactSelect {...props} />
+      <ReactSelect
+        unstyled
+        classNames={{
+          container: ({ isFocused }) =>
+            clsx(
+              'rounded-md border border-input bg-transparent px-3 text-sm shadow-sm transition-colors',
+              isFocused ? 'outline-none ring-1 ring-ring' : ''
+            ),
+          option: () => clsx('p-2 hover:bg-secondary'),
+          menu: () => clsx('-ml-2 mt-2 rounded-md border bg-white'),
+          multiValue: () =>
+            clsx(
+              'rounded-md border border-primary/60 bg-primary-foreground px-2 py-1'
+            )
+        }}
+        {...props}
+      />
     </div>
   )
 }
