96243b9af86906b013ed97d0ee6b1d2a131436fe
estefane.cortes <estefanecortes@gmail.com>
2024-12-15 18:37:59 -0300

    refactor: remove search from header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,10 @@
 import { AppName } from '../AppName'
-import { Input } from '../ui/input'
 import { NewTaskModal } from '../Task/NewTaskModal'
 
 export function Header() {
   return (
     <header className="relative z-20 flex min-h-header items-center justify-between gap-4 border-b  p-4">
       <AppName size="small" />
-      <Input type="text" placeholder="Search tasks..." />
 
       <NewTaskModal />
     </header>
