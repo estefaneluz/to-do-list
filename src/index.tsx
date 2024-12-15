5d57f98b1adb3588d62c89ae27fa5caee6b9f34d
estefane.cortes <estefanecortes@gmail.com>
2024-12-15 20:23:56 -0300

    chore: configure react query

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,18 @@
-import { createRoot } from 'react-dom/client'
-import 'tailwindcss/tailwind.css'
 import App from '@/pages/App'
+import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
+import { Providers } from './providers'
+
 import '@/index.css'
+import 'tailwindcss/tailwind.css'
 
 const container = document.getElementById('root') as HTMLDivElement
 const root = createRoot(container)
 
 root.render(
   <BrowserRouter>
-    <App />
+    <Providers>
+      <App />
+    </Providers>
   </BrowserRouter>
 )
