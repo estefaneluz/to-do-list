c0cc8de358851bb3d8eecac5e698b7666dcc7296
estefane.cortes <estefane.cortes@aquare.la>
2024-12-16 16:22:05 -0300

    feat: create tasks query

--- src/hooks/queries/use-get-tags.tsx
+++ src/hooks/queries/use-get-tags.tsx
@@ -1,6 +1,7 @@
-import { useQuery } from '@tanstack/react-query'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import client from '@/api/clients'
 import { Tag } from '@/types/tag'
+import { toast } from '../use-toast'
 
 async function getAllTags(): Promise<Tag[]> {
   const { data } = await client.get('/tags/')
@@ -12,5 +13,37 @@ export function useGetTags() {
   return useQuery({
     queryKey: ['ALL_TAGS'],
     queryFn: getAllTags
+  })
+}
+
+type CreateTag = {
+  name: string
+  hex_color: string
+}
+
+async function createTag(payload: CreateTag): Promise<Tag> {
+  const { data } = await client.post<Tag>('/tags/', payload)
+  return data
+}
+
+export const useCreateTag = () => {
+  const queryClient = useQueryClient()
+
+  return useMutation({
+    mutationFn: createTag,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['ALL_TAGS'] })
+      toast({
+        title: 'Success!',
+        description: 'You have successfully created a tag.'
+      })
+    },
+    onError: () => {
+      toast({
+        title: 'Error!',
+        description: 'Something went wrong.',
+        variant: 'destructive'
+      })
+    }
   })
 }
--- src/pages/AllTasks/NewTagModal/index.tsx
+++ src/pages/AllTasks/NewTagModal/index.tsx
@@ -7,14 +7,55 @@ import {
 } from '@/components/ui/dialog'
 import { Button } from '@/components/ui/button'
 import { Plus } from 'lucide-react'
-import { InputWithLabel } from '@/components/Fields/InputWithLabel'
 import { badgeVariants } from '@/components/ui/badge'
 import { DialogDescription } from '@radix-ui/react-dialog'
 import clsx from 'clsx'
+import { Form } from '@/components/ui/form'
+import { ControlledInput } from '@/components/Fields/ControlledInput'
+import { useCreateTag } from '@/hooks/queries/use-get-tags'
+import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import yup from '@/api/yup'
+import { useEffect, useState } from 'react'
+import { AxiosError } from 'axios'
 
 export function NewTagModal() {
+  const [open, setOpen] = useState(false)
+
+  const NewTag = yup.object({
+    name: yup.string().required(),
+    hex_color: yup.string().required()
+  })
+
+  const form = useForm({
+    resolver: yupResolver(NewTag)
+  })
+
+  const { mutate: createTag, isPending: isCreating } = useCreateTag()
+
+  useEffect(() => {
+    if (!open) form.reset()
+  }, [form, open])
+
+  const onSubmit = (data: yup.InferType<typeof NewTag>) => {
+    createTag(data, {
+      onSuccess() {
+        setOpen(false)
+      },
+      onError(error) {
+        if (error instanceof AxiosError) {
+          Object.entries(error.response?.data).forEach(([key, value]) => {
+            form.setError(key as 'name' | 'hex_color', {
+              message: value as string
+            })
+          })
+        }
+      }
+    })
+  }
+
   return (
-    <Dialog>
+    <Dialog open={open} onOpenChange={setOpen}>
       <DialogTrigger asChild>
         <Button
           variant="secondary"
@@ -32,24 +73,35 @@ export function NewTagModal() {
           </DialogDescription>
         </DialogHeader>
 
-        <form className="flex w-full max-w-lg flex-col gap-5">
-          <InputWithLabel
-            label="Name"
-            type="text"
-            placeholder="Describe your tag"
-          />
+        <Form {...form}>
+          <form
+            className="flex w-full max-w-lg flex-col gap-5"
+            onSubmit={form.handleSubmit(onSubmit)}
+          >
+            <ControlledInput
+              label="Name"
+              name="name"
+              type="text"
+              placeholder="Describe your tag"
+            />
 
-          <InputWithLabel
-            className="max-w-20"
-            label="Color"
-            type="color"
-            placeholder="Describe your tag"
-          />
+            <ControlledInput
+              className="max-w-20"
+              name="hex_color"
+              label="Color"
+              type="color"
+              placeholder="Describe your tag"
+            />
 
-          <Button type="button" className="h-10 w-full text-sm">
-            Create
-          </Button>
-        </form>
+            <Button
+              disabled={isCreating}
+              type="submit"
+              className="h-10 w-full text-sm"
+            >
+              Create
+            </Button>
+          </form>
+        </Form>
       </DialogContent>
     </Dialog>
   )
