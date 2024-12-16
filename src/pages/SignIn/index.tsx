eceb768e63690b3f8a974d27d7a81ac803f6f2e8
estefane.cortes <estefane.cortes@aquare.la>
2024-12-16 11:14:27 -0300

    feat: sign in request

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,13 +1,39 @@
-import { AppName } from '@/components/AppName'
-import { InputWithLabel } from '@/components/Fields/InputWithLabel'
-import { Button } from '@/components/ui/button'
+import yup from '@/api/yup'
 import { useNavigate } from 'react-router'
 
+import { AppName } from '@/components/AppName'
+import { Button } from '@/components/ui/button'
+import { ControlledInput } from '@/components/Fields/ControlledInput'
+import { Form } from '@/components/ui/form'
+import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { useSignIn } from '@/hooks/queries/use-authentication'
+
 const SignIn = () => {
+  const LoginUser = yup.object({
+    email: yup.string().email().required(),
+    password: yup.string().required()
+  })
+
+  const form = useForm({
+    resolver: yupResolver(LoginUser)
+  })
+
+  const { mutate: signIn } = useSignIn()
+
   const navigate = useNavigate()
 
   const goToSignUp = () => {
     navigate('/signup')
+  }
+
+  const onSubmit = (data: yup.InferType<typeof LoginUser>) => {
+    signIn({
+      payload: {
+        email: data.email,
+        password: data.password
+      }
+    })
   }
 
   return (
@@ -18,34 +44,40 @@ const SignIn = () => {
         <h2 className="text-3xl font-bold">Welcome back</h2>
         <p className="text-gray-600">Please enter your details to sign in</p>
       </div>
+      <Form {...form}>
+        <form
+          className="flex w-full max-w-lg flex-col gap-6"
+          onSubmit={form.handleSubmit(onSubmit)}
+        >
+          <ControlledInput
+            label="Email"
+            name="email"
+            type="email"
+            placeholder="Enter your email"
+          />
 
-      <form className="flex w-full max-w-lg flex-col gap-6">
-        <InputWithLabel
-          label="Email"
-          type="email"
-          placeholder="Enter your email"
-        />
+          <ControlledInput
+            label="Password"
+            name="password"
+            type="password"
+            placeholder="Enter your password"
+          />
 
-        <InputWithLabel
-          label="Password"
-          type="password"
-          placeholder="Enter your password"
-        />
+          <Button className="h-12 text-base">Sign In</Button>
 
-        <Button className="h-12 text-base">Sign In</Button>
-
-        <div className="flex items-center justify-center gap-1 text-sm">
-          <p>New user?</p>
-          <Button
-            type="button"
-            className="p-0 text-sm"
-            variant="link"
-            onClick={goToSignUp}
-          >
-            Create an account
-          </Button>
-        </div>
-      </form>
+          <div className="flex items-center justify-center gap-1 text-sm">
+            <p>New user?</p>
+            <Button
+              type="button"
+              className="p-0 text-sm"
+              variant="link"
+              onClick={goToSignUp}
+            >
+              Create an account
+            </Button>
+          </div>
+        </form>
+      </Form>
     </div>
   )
 }
