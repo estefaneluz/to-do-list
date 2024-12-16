61d683f4b778918783ab77f3f25d9f03e049b240
estefane.cortes <estefane.cortes@aquare.la>
2024-12-16 11:13:50 -0300

    feat: controlled input component

--- src/api/clients/index.ts
+++ src/api/clients/index.ts
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
+const baseURL = import.meta.env.VITE_API_URL
+
 const client = axios.create({
-  baseURL: '',
+  baseURL,
   timeout: 10000,
   headers: { 'Content-Type': 'application/json' }
 })
