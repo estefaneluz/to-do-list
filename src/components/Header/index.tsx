b5cd62fc1b36e6982713a1ec2b575b882f9d19ea
estefane.cortes <estefane.cortes@aquare.la>
2024-12-16 22:47:18 -0300

    style: set header background color to white

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,7 +3,7 @@ import { NewTaskModal } from '../Task/NewTaskModal'
 
 export function Header() {
   return (
-    <header className="relative z-20 flex min-h-header items-center justify-between gap-4 border-b  p-4">
+    <header className="relative z-20 flex min-h-header items-center justify-between gap-4 border-b bg-white p-4">
       <AppName size="small" />
 
       <NewTaskModal />
